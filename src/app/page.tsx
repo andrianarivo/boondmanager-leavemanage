d09ec10edbba8eb3fbff80a7e7b3b4bd84921619
david <andrianarivo.david@gmail.com>
2024-01-20 23:11:38 +0300

    chore: change editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import * as actions from '@/actions'
-import {Button} from '@nextui-org/react'
+import { Button } from '@nextui-org/react'
 
 export default function Home() {
   return (
-    <main className='flex justify-center items-center'>
+    <main className="flex justify-center items-center">
       <form action={actions.signIn}>
-        <Button type='submit' color='primary' variant='bordered'>Sign In</Button>
+        <Button type="submit" color="primary" variant="bordered">
+          Sign In
+        </Button>
       </form>
     </main>
   )
--- src/auth.ts
+++ src/auth.ts
@@ -4,23 +4,28 @@ import GoogleProvider from 'next-auth/providers/google'
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
-if(!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   throw new Error('Missing Google credentials')
 }
 
-export const { handlers : { GET, POST }, auth, signOut, signIn } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
   providers: [
-      GoogleProvider({
-        clientId: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET
-      })
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
   ],
   callbacks: {
-    async session({session, user}: any) {
-      if(session && user) {
+    async session({ session, user }: any) {
+      if (session && user) {
         session.user.id = user.id
       }
       return session
-    }
-  }
+    },
+  },
 })
